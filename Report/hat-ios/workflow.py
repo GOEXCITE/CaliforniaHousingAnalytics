2bc6bbe78b555ede715a296c9219c372fa80cb1e
Tian Tian <HollyTian@Tians-MacBook-Pro.local>
2017-12-03 14:17:09 +0900

    change file tree

--- Report/hat-ios/workflow.py
+++ Report/hat-ios/workflow.py
@@ -3,22 +3,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-DATA_PATH = os.path.join("data", "Report1550_e.csv")
+DATA_PATH = os.path.join("data/hat-ios", "Report1550_e.csv")
 
-rawData = pd.DataFrame.from_csv(DATA_PATH)
+rawData = pd.read_csv(DATA_PATH)
 
-# rawData.info()
+rawData.info()
 
 # rawData.dropna(subset=['SHOW'])
 # rawData.describe()
 # rawData.hist(bins=50, figsize=(20, 15))
 # plt.show()
 
-for i in range(len(rawData)):
-    if rawData.ix[i, 'evar96'] == 'SHOW':
-        rawData = rawData.drop(i)
-
-rawData.info()
+# for i in range(len(rawData)):
+#     if rawData.ix[i, 'evar96'] == 'SHOW':
+#         rawData = rawData.drop(i)
+#
+# rawData.info()
 
 # checkNotShowData = rawData.copy()
 #
